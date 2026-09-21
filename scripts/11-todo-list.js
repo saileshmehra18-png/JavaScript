ca88a091143df182c220bb60ca6521010840325f
Sailesh <Saileshmehra18@gmail.com>
2026-09-21 22:01:43 +0530

    did part fo arrays and loops make the todo list better and also added some css in it so it look good too

--- scripts/11-todo-list.js
+++ scripts/11-todo-list.js
@@ -1,6 +1,9 @@
-const myArray = ['make dinner',
-                'wash dishes ',
-                'watch youtune'];
+const myArray = [{name:'make dinner',
+                date:'24,5,5332'},
+
+                {name:'wash dishes ',
+                date:'53,3,3342'}
+                ];
 
 rendermyArray();
 
@@ -8,22 +11,43 @@ function rendermyArray(){
     let myArrayHTML = ''
 
     for (let i = 0; i<myArray.length; i++){
-        const todo = myArray[i];
-        const html = `<p>${todo}</p>`
+        const todoObject = myArray[i];
+        const name = todoObject.name;
+        const due = todoObject.date;
+        
+        const html=
+            `<div>${name}</div>
+             <div>${due}</div>
+            <button class="delete-todo-button" onclick="
+                myArray.splice(${i},1);
+                rendermyArray();
+            ">Delete</button>`
         myArrayHTML += html
     }
 
-    document.querySelector('.show-text').innerHTML=
+    document.querySelector('.js-show-text').innerHTML=
     myArrayHTML;
     
 }
 
 function addArray() {
     const input = document.querySelector('.js-input');
-    let text = input.value;
 
-    myArray.push(text)
-    console.log(myArray);
+    const dateElement = document.querySelector('.js-date-input');
+    
+    let name = input.value;
+    let date = dateElement.value;
+
+    myArray.push({
+        // text: text,
+        //  date:date,
+        name,
+        date
+        //both the ways are correct this is shortcut
+        }
+
+
+    )
 
     input.value = '';
     rendermyArray();
